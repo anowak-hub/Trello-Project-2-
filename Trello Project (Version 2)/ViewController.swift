dee0a65f3042aeefe63fee4d45fe5ffb55f56a2f
anowak-hub <anowak4320@stu.d214.org>
2022-01-10 11:06:31 -0600

    Wrote and used function of "getData"

--- Trello Project (Version 2)/ViewController.swift
+++ Trello Project (Version 2)/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -22,6 +23,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         trelloTableView.dataSource = self
         trelloTableView.delegate = self
         
+        trelloTableView.reloadData()
+        getData()
     }
 // MARK: - Functions, Actions, etc.
 
@@ -42,6 +45,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 //        trelloTableView.reloadData()
     }
     
-    
+    public func getData() {
+        items.removeAll()
+        
+        let database = Database.database().reference().child("Items")
+        database.observeSingleEvent(of: .value) { snapshot in
+            for data in snapshot.children.allObjects as! [DataSnapshot] {
+                let item = Item()
+                let name = data.value as! String
+                
+                item.name = name
+                self.items.append(item)
+                
+            }
+        }
+    }
 }
 
